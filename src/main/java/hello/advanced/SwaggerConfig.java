143b05cd4bfc0ef7c32fd5d04179640029feac14
CODe1995 <code.57x53@gmail.com>
2022-04-19 20:43:04 +0900

    feat: swagger 설정 추가

---
+++ src/main/java/hello/advanced/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package hello.advanced;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("hello.advanced"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Practice spring")
+                .description("practice spring with swagger")
+                .version("0.0")
+                .build();
+    }
+}
